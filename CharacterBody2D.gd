5fcab79492e1eb6b6135b52a5a3cafdde6b419c1
mittens27 <amluck88@gmail.com>
2024-02-27 16:51:49 -0500

    added player sprite with normal map and collision. added boilerplate script.

---
+++ CharacterBody2D.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+
+func _physics_process(delta):
+
+	var direction = Input.get_axis("up", "down")
+	if direction:
+		velocity.y = direction * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	move_and_slide()
